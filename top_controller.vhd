3c90870bbaabf6b53f37188233685aadf4acada8
LotannaEzenwa <lotanna.m.ezenwa.17@dartmouth.edu>
2016-01-22 22:54:24 -0600

    Revert "Revert "Removed everything that was unnecessary""
    This reverts commit 2695d05b3158d661752928444d13981b98b2c581.

---
+++ top_controller.vhd
@@ -0,0 +1,42 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:19:56 08/20/2015 
+-- Design Name: 
+-- Module Name:    top_controller - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top_controller is
+    Port ( clk : in  STD_LOGIC);
+end top_controller;
+
+architecture Behavioral of top_controller is
+
+begin
+
+
+end Behavioral;
+
